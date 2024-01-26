0251ea4bf1a7079d2f131fe903aefd537d00f2b7
Hritik Tailor <hritiktailor@gmail.com>
2024-01-26 21:56:34 +0000

    added volumeofcylinder function

--- week1.hs
+++ week1.hs
@@ -6,5 +6,10 @@ timesTen n = n * 10
 sumThree :: Int -> Int -> Int -> Int
 sumThree a b c = a + b + c
 
+-- 3
 areaOfCircle :: Float -> Float
 areaOfCircle area = pi * (area ^ 2)
+
+-- 4
+volumeOfCylinder :: Float -> Float -> Float
+volumeOfCylinder h r = pi * r^2 * h
