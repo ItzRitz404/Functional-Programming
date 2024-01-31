1c8235667c2fc973e3228b970a47bd1ae7bca6cb
Hritik Tailor <hritiktailor@gmail.com>
2024-01-31 12:10:26 +0000

    improvements

--- week1.hs
+++ week1.hs
@@ -8,15 +8,16 @@ sumThree a b c = a + b + c
 
 -- 3
 areaOfCircle :: Float -> Float
-areaOfCircle area = pi * (area ^ 2)
+areaOfCircle r = pi * r ^ 2
 
 -- 4
 volumeOfCylinder :: Float -> Float -> Float
-volumeOfCylinder h r = pi * r^2 * h
+-- volumeOfCylinder h r = pi * r ^ 2 * h
+volumeOfCylinder h r = areaOfCircle r * h
 
 -- 5
 distance :: Float -> Float -> Float -> Float -> Float
-distance x1 y1 x2 y2 = sqrt ((y1 - y2)^2 + (x1 - x2)^2)
+distance x1 y1 x2 y2 = sqrt ((y1 - y2) ^ 2 + (x1 - x2) ^ 2)
 
 -- 6
 threeDifferent :: Int -> Int -> Int -> Bool
@@ -24,7 +25,8 @@ threeDifferent a b c = a /= b && c /= b && a /= c
 
 -- 7
 divisibleBy :: Int -> Int -> Bool
-divisibleBy a b = a `mod` b == 0
+-- divisibleBy a b = a `mod` b == 0
+divisibleBy a b = mod a b == 0
 
 -- 8
 isEven :: Int -> Bool
@@ -32,8 +34,8 @@ isEven = even
 
 -- 9
 averageThree :: Int -> Int -> Int -> Float
-averageThree a b c = (fromIntegral a + fromIntegral b + fromIntegral c) / 3
+averageThree a b c = fromIntegral (a + b + c) / 3
 
 -- 10
-absolute :: Int -> Int 
+absolute :: Int -> Int
 absolute a = if a >= 0 then a else -a
